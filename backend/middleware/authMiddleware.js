658699212ea5264b7ea5894aed3fe5cdfa7c43d9
Citizshrestha <citizshresthaa@gmail.com>
2025-05-10 21:37:46 +0545

    asyncHandler used

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,29 @@
-import jwt from "jsonwebtoken";
-import {User} from "../models/user.models.js";
+// authMiddleware.js
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.models.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
 
-const protect = async (req, res, next) => {
-
+const protect = asyncHandler(async (req, res, next) => {
   let token;
-  if ( req.headers.authorization && req.headers.authorization.startsWith("Bearer") ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User
-                      .findById(decoded.id)
-                      .select("-password");
-                      
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
       if (!req.user) {
-        return res.status(401).json({ message: "User not found" });
+        return res.status(401).json({ success: false, message: 'User not found' });
       }
       next();
     } catch (error) {
-      res.status(401).json({ message: "Not authorized, token failed" });
-    }
-  } else {
-    res.status(401).json({ message: "Not authorized, no token" });
+  if (error.name === "TokenExpiredError") {
+    return res.status(401).json({ success: false, message: "Access token expired" });
   }
-};
+  return res.status(401).json({ success: false, message: "Invalid access token" });
+}
+  }
+ else {
+    return res.status(401).json({ success: false, message: 'No access token provided, token missing' });
+  }
+});
 
 export default protect;

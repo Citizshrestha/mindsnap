2e9ca03f4f2a7ef0252b5a16987dbc0622e38136
Citizshrestha <citizshresthaa@gmail.com>
2025-08-28 18:52:04 +0545

    Redux tool kit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,15 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import {GoogleOAuthProvider} from '@react-oauth/google'
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='663069197970-jk03o0o9oje0si598d7af9cpl8oik6o4.apps.googleusercontent.com'>
-
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="663069197970-jk03o0o9oje0si598d7af9cpl8oik6o4.apps.googleusercontent.com">
+      <Provider store={store}> 
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </GoogleOAuthProvider>
-
   </React.StrictMode>
 );

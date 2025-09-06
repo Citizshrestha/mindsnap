f40da4557675dc09fca5e2d106c4c5c27cfa90bd
Citizshrestha <citizshresthaa@gmail.com>
2025-09-06 23:49:39 +0545

    headers for coops and cors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
   define: {
     'process.env.NODE_ENV': JSON.stringify(mode),
   },
+  server: {
+    headers: {},
+  },
   build: {
     rollupOptions: {
-      external: ['emoji-mart']
-    }
-  }
+      external: [],
+    },
+  },
 }));
-

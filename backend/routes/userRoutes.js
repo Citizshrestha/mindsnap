e310087cd3ffa5ea0386e7b62ffbf064ecc2c51d
Citizshrestha <citizshresthaa@gmail.com>
2025-09-16 22:43:09 +0545

    follow back user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import { getUserProfileInfo, updateUserProfile, generateSignature, searchUsers, followUser, unfollowUser, getUserById, getUserConnections } from "../controllers/userController.js";
+import { getUserProfileInfo, updateUserProfile, generateSignature, searchUsers, followUser, unfollowUser, getUserById, getUserConnections, followBackUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.get("/signature", protect, generateSignature);
 
 router.get("/search", protect, searchUsers);
 router.post("/:id/follow", protect, followUser);
+router.post("/:id/follow-back", protect, followBackUser);
 router.post("/:id/unfollow", protect, unfollowUser);
 router.get("/:userId", protect, getUserById); 
 router.get("/profile/connections", protect, getUserConnections); 
